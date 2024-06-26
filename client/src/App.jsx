51e8c6eac0ad04183b5df92dd2c2bd5e8a40e846
Dufourneau Anais <anais.dufourneau87@gmail.com>
2024-06-26 14:47:38 +0200

    add root in CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
 function App() {
-  return <h1>Geo Quest</h1>;
+  return (
+  <>
+  <h1>Geo Quest</h1>
+  <p>Année 3452, l'inspecteur " User.name " , inspecteur de renom est invité par la Centrale afin d'élucider l'affaire de ces deux dernières décénies. Un tueur en série s'est emparé d'une capsule spatio temporelle afin de parcourir les époques pour semer le chaos, assassinant divers personnages historiques et altérer le monde tel que nous le connaissons. Parviendra-t'il à retrouver les indices et découvrir le nom du meurtirer?
+
+  </p>
+<p>"  Ah, te voilà! C'est toi le nouvel inspecteur que la centrale nous envoie?  Dépèche-toi, l'équipage est déjà sur le départ. 
+ Quelle est la première étape? 
+ Direction l'Egypte ancienne, nos traqueurs temporels nous indiques que la cible s'est rendu à cet endroit pour débuter ses méfaits. Nous devons absolument le retrouver et réparer les dégats! 
+ Entendu, allons-voir ça de plus près. "</p>
+  </>
+)
 }
 
 export default App;
