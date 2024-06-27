57a628606a06cd7cf1d78bd556fc4003428987c7
Kevin Peset <kevin.peset@gmail.com>
2024-06-27 11:16:36 +0200

    Modification du App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,12 @@ import Quizz from "./components/Quizz";
 import HomePage from "./components/HomePage";
 import ClickEffect from "./components/ClickEffect";
 
-
 function App() {
   return (
     <main>
       <Quizz />
       <HomePage />
-      <ClickEffect handleAnswerClick={handleAnswerClick} />
+      <ClickEffect />
     </main>
   );
 }
