152b573de63c5aa89e643cbbaa7dc26af7629c65
Pyriac <cyriacpineau@gmail.com>
2024-07-01 16:15:23 +0200

    light in green good answer and in red bad answer

--- client/src/components/AnimationButton.jsx
+++ client/src/components/AnimationButton.jsx
@@ -3,19 +3,26 @@ import { motion } from "framer-motion";
 import PropTypes from "prop-types";
 import "../style/buttonBlood.css";
 
-export default function AnimationButton({ dataName }) {
+export default function AnimationButton({ dataName, goodAnswer }) {
   const [showImage, setShowImage] = useState(false);
+  const [className, setClassName] = useState("button");
 
   const handleClick = () => {
     setShowImage(true);
     setTimeout(() => {
       setShowImage(false);
     }, 1000);
+    if (dataName === goodAnswer) {
+      setClassName("greenButton");
+    } else {
+      setClassName("redButton");
+    }
+    setTimeout(() => setClassName("button"), 1000);
   };
 
   return (
     <div className="animation-button">
-      <button className="button" type="button" onClick={handleClick}>
+      <button className={className} type="button" onClick={handleClick}>
         {dataName}
       </button>
       {showImage && (
@@ -35,4 +42,5 @@ export default function AnimationButton({ dataName }) {
 
 AnimationButton.propTypes = {
   dataName: PropTypes.arrayOf(PropTypes.string).isRequired,
+  goodAnswer: PropTypes.string.isRequired,
 };
--- client/src/components/ClickEffect.jsx
+++ client/src/components/ClickEffect.jsx
@@ -2,15 +2,17 @@ import PropTypes from "prop-types";
 import AnimationButton from "./AnimationButton";
 
 export default function ClickEffect({ dataName }) {
+  const goodAnswer = dataName[0];
+  console.info(goodAnswer);
   return (
     <div className="answers-class1">
       <div className="answers">
-        <AnimationButton dataName={dataName[0]} label="Réponse 1" />
-        <AnimationButton dataName={dataName[1]} label="Réponse 2" />
+        <AnimationButton dataName={dataName[0]} goodAnswer={goodAnswer} />
+        <AnimationButton dataName={dataName[1]} goodAnswer={goodAnswer} />
       </div>
       <div className="answers">
-        <AnimationButton dataName={dataName[2]} label="Réponse 3" />
-        <AnimationButton dataName={dataName[3]} label="Réponse 4" />
+        <AnimationButton dataName={dataName[2]} goodAnswer={goodAnswer} />
+        <AnimationButton dataName={dataName[3]} goodAnswer={goodAnswer} />
       </div>
     </div>
   );
