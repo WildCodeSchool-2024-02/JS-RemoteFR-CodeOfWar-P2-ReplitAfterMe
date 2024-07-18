8930cfe308baee255e58741727735ac312178c55
Dufourneau Anais <anais.dufourneau87@gmail.com>
2024-07-18 14:47:54 +0200

    continued add story.js

--- client/src/data/story.js
+++ client/src/data/story.js
@@ -12,45 +12,89 @@ const story = [
   },
   {
     chapter: 2,
-    text: `chapter2`,
+    text: `<strong>B</strong>ercé par la lueur d'une lune froide comme un cœur en deuil, "User-name" entre dans le Colisée l'esprit hagard.<br /><br />
+          <strong>E</strong>n explorant les ruines, l'oxygène semblait lui manquer, comme un poumon percé. Quand soudain, un médaillon accroché sur une pierre attira son attention.<br /><br />
+          <strong>L</strong>a gravure au dos indiquait " La logique, c'est logique ", une citation ancestrale prononçait par une philosophe Française un lendemain d'Hakathon.<br /><br />
+          <strong>G</strong>alvanisé par cette chasse, "User-name" esquissa un sourire face à cet indice superflus, se disant qu'il aurait pu y être inscrit "Toto" cela n'aurait rien changé. <br /><br />
+          <strong>E</strong>mportant le médaillon, l'inspecteur fit son rapport à la Centrale et reprit la route en suivant les traces laissées par sa proie.`,
   },
   {
     chapter: 3,
-    text: `
-    
-    <strong>B</strong>ercé par la lueur d'une lune froide comme un cœur en deuil, "User-name" entre dans le Colisée l'esprit hagard.<br /><br />
-    <strong>E</strong>n explorant les ruines, l'oxygène semblait lui manquer, comme un poumon percé. Quand soudain, un médaillon accroché sur une pierre attira son attention.<br /><br />
-    <strong>L</strong>a gravure au dos indiquait " La logique, c'est logique ", une citation ancestrale prononçait par une philosophe Française un lendemain d'Hakathon.<br /><br />
-    <strong>G</strong>alvanisé par cette chasse, "User-name" esquissa un sourire face à cet indice superflus, se disant qu'il aurait pu y être inscrit "Toto" cela n'aurait rien changé. <br /><br />
-    <strong>E</strong>mportant le médaillon, l'inspecteur fit son rapport à la Centrale et reprit la route en suivant les traces laissées par sa proie.`,
+    text: `<strong>P</strong>récieux fragments de marbre, fragments de vies perdues dans les méandres du temps.<br /><br />
+          <strong>R</strong>évélation de secrets enfouis, ornés de symboles anciens, énigmes inscrites sur la pierre du destin.<br /><br />
+          <strong>O</strong>des muettes des dieux antiques, murmures gravés dans les plis du marbre érodé.<br /><br />
+          <strong>F</strong>raction d'une sagesse éternelle, scellée dans la fissure des âges oubliés.<br /><br />
+          <strong>E</strong>nvolées mystiques, où les ombres dansent autour des colonnes immortelles.<br /><br />
+          <strong>S</strong>ouvenirs figés dans le silence, échos d'une époque où les héros et les dieux se croisaient.<br /><br />
+          <strong>S</strong>culptures du passé, qui chuchotent des vérités qui blessent enfouies sous le regard de la lune.<br /><br />
+          <strong>É</strong>clats de lumière, révélateurs d'une histoire enfouie dans les plis de la mémoire antique.<br /><br />
+          <strong>U</strong>nissant les temps, où l'art et l'histoire fusionnent en un hymne éternel.<br /><br />
+          <strong>R</strong>êves de marbre, où réside l'essence immuable de la Grèce éternelle.`,
   },
   {
     chapter: 4,
-    text: `chapter4`,
+    text: `<strong>R</strong>emontant les couloirs d'un château où les cris raisonnent, "User.name" écoute, les douleurs passées.<br /><br />
+          <strong>U</strong>ne lettre mise en évidence sur une porte en bois, écrite en ancien français l'attends.<br /><br />
+          <strong>B</strong>ravant les obstacles du temps, elle révèle des récits de chevaliers et de noblesse.<br /><br />
+          <strong>I</strong>lluminée par la lueur des chandelles, elle mentionne un indice précieux. Une phrase interpelle "User.name".<br /><br />
+          "<strong>S</strong>uis-je l'éloquence d'un sourd, le dresseur de mes peines, l'ennemi de l'amour, car je suis amoureux de la haine? ”`,
   },
   {
     chapter: 5,
-    text: `chapter5`,
+    text: `<strong>G</strong>rimpant les marches usées du palais florentin, "User.name" s'interrogeait.<br /><br /> 
+          <strong>U</strong>n halo lumineux berçait le mur de pierres face à lui.<br /><br />
+          <strong>I</strong>l savait, que chaque trait dissimulait une vérité cachée,<br /><br />
+          <strong>T</strong>apis dans l'ombre des œuvres, des secrets à dévoiler,<br /><br />
+          <strong>A</strong>veuglés par l'éclat des chefs-d'œuvre, ses pensées s'égarèrent,<br /><br />
+          "<strong>R</strong>espire et penses, inspires et panses, expires et transformes le tout en expérience"<br /><br />
+          <strong>I</strong>l se répétait... comme un mantra lui permettant de se canaliser et garder l'esprit vif.<br /><br />
+          <strong>S</strong>'efforçant de comprendre, l'atypique personnalité de cet être vil et vicieux.<br /><br />
+          <strong>T</strong>issant des fils d'histoires entrelacées, inspirés et guéris,<br /><br />
+          <strong>E</strong>n explorant les méandres des abysses de son âme, la clarté l'investissait.`,
   },
   {
     chapter: 6,
-    text: `chapter6`,
+    text: `<strong>C</strong>omme une ombre parmi les pavés ensanglantés, où chaque roche eu croiser la Méduse,<br /><br />
+          <strong>A</strong>ux cris étouffés des révoltés et des condamnés, "User.name" écouta dans le calme,<br /><br />
+          <strong>N</strong>ul ne peut ignorer l'aura assassine, l'intention du malin et l'odeur de la poudre.<br /><br />
+          <strong>A</strong>ux feux de la rébellion, dans les cendres nacrées du passé, le miel amer,<br /><br />
+          <strong>R</strong>évélant les secrets d'une nation en effervescence, d'une intention convaincue.<br /><br />
+          <strong>D</strong>ans l'obscurité, l'inspecteur poursuivit ses recherches...`,
   },
   {
     chapter: 7,
-    text: `chapter7`,
+    text: `<strong>S</strong>ous le crépitement des armes, dans le silence des ruines où les souvenirs s'effacent,<br /><br />
+          <strong>T</strong>raqué par les ombres du passé, un chemin tortueux se dessine à travers les décombres,<br /><br />
+          <strong>A</strong>ux confins de la mémoire, tracés de lignes et d'étoiles, tels des indices éparpillés,<br /><br />
+          <strong>R</strong>évélations murmurées sur des cartes jaunies par le temps, chaque pli une histoire,<br /><br />
+          <strong>A</strong>u cœur des stratégies, les mouvements se dévoilent, comme les constellations dans le ciel nocturne,<br /><br />
+          <strong>C</strong>ontournant l'obscurité, où chaque étoile est une étincelle fugace dans l'abîme de l'oubli.`,
   },
   {
     chapter: 8,
-    text: `chapter8`,
+    text: `<strong>V</strong>oix perdues dans l'éther, l'écho d'un voyages sans retour,<br /><br />
+          <strong>E</strong>nregistré dans les cieux, les messages cryptés murmurent en fourchelang,<br /><br />
+          <strong>L</strong>oin des étoiles, dans les méandres de la technologie,<br /><br />
+          <strong>K</strong>afkaïennes sont les pistes qu'User.name" suit sans répit.<br /><br />
+          <strong>O</strong>mbres sur l'astre lumineux et froid, au milieu des étoiles filantes et des illusions,<br /><br />
+          <strong>Z</strong>éphyr d'un meurtrier cosmique, qui laisse sa marque sans nom, ses empreintes sans traces.`,
   },
   {
     chapter: 9,
-    text: `chapter9`,
+    text: `<strong>K</strong>aleidoscope de mystères, où les ombres dansent sous les colonnes anciennes.<br /><br />
+          <strong>É</strong>chos des prophéties gravées, murmures des dieux en sommeil, "User.name" se rapproche et le sent<br /><br />
+          <strong>V</strong>ision envoûtante des destinées entrelacées, qui révèle le fil d'or du destin.<br /><br />
+          <strong>I</strong>ncertitudes du passé, révélations cryptées dans les runes de pierre et d'éternité.<br /><br />
+          <strong>N</strong>uits étoilées, où le temps entrelace ses secrets à travers le voile des âges.
+`,
   },
   {
     chapter: 10,
-    text: `chapter10`,
+    text: `<strong>P</strong>ensées égarées dans les labyrinthes numériques, où les âmes se perdent dans les fils du code.<br /><br />
+          <strong>É</strong>chos de vies artificielles, spectres de l'humanité transfigurée dans les circuits virtuels.<br /><br />
+          <strong>S</strong>ilence des mégapoles, résonance de l'effondrement des rêves au rythme des battements de cœur artificiels.<br /><br />
+          <strong>E</strong>nigmes de lumière, pixels de vérité et d'illusion se fondent dans l'éther numérique.<br /><br />
+          <strong>T</strong>énèbres du futur, où les souvenirs se dissolvent dans l'obscurité des horloges à retardement.`,
   },
 ];
 
